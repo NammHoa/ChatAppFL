89afeaf8efe25501a55de65a9fde4199c79168f0
NammHoa <namdeptrai270304@gmail.com>
2025-05-21 23:20:51 +0700

    push-first

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:chatappflutter/main_screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.savedThemeMode});
+  final AdaptiveThemeMode? savedThemeMode;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.purple,
+      ),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.purple,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/main_screen/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:chatappflutter/main_screen/chats_list_screen.dart';
+import 'package:chatappflutter/main_screen/groups_screen.dart';
+import 'package:chatappflutter/main_screen/people_screen.dart';
+import 'package:chatappflutter/utilities/assets_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController(initialPage: 0);
+  int currentIndex = 0;
+
+  final List<Widget> pages = const [
+    ChatsListScreen(),
+    GroupsScreen(),
+    PeopleScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage(AssetsManager.userImage),
+            ),
+          ),
+        ],
+      ),
+
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        children: pages,
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble_2_fill),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.group),
+            label: 'Group',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.globe),
+            label: 'People',
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (index) {
+          pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
